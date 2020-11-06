ec61bc0f747247c195740489564482d15fac7bd9
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-07 04:07:54 +0530

    [Akshay] Added Classes For New Schema

---
+++ EmployeePayrollProblem/New Schema/Department.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollProblem.New_Schema
+{
+    class Department
+    {
+        public int DepartmentID { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
--- EmployeePayrollServiceMSTest/TestEmployeeDBOperations.cs
+++ EmployeePayrollServiceMSTest/TestEmployeeDBOperations.cs
@@ -9,6 +9,7 @@ namespace EmployeePayrollServiceMSTest
     using Microsoft.VisualStudio.TestTools.UnitTesting;
     using EmployeePayrollProblem;
     using System.Security.Cryptography;
+    using System;
 
     [TestClass]
     public class TestEmployeeDBOperations
@@ -23,6 +24,31 @@ namespace EmployeePayrollServiceMSTest
             // Act
             double actual = EmployeeDBOperations.GetSalary(name);
             double expected = basicPay;
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestInsertEmployee()
+        {
+            // Arrange
+            Employee employee = new Employee();
+            employee.EmployeeID = 0;
+            employee.EmployeeName = "Akshay";
+            employee.StartDate = Convert.ToDateTime("2020-01-01");
+            employee.Gender = "M";
+            employee.PhoneNumber = "8094144527";
+            employee.Address = "KZH";
+            employee.Department = "SDE";
+            employee.BasicPay = 25000;
+            employee.Deductions = 0;
+            employee.TaxablePay = 0;
+            employee.IncomeTax = 0;
+            employee.NetPay = 0;
+            // Act
+            EmployeeDBOperations.InsertEmployeeDetails(employee);
+            double actual = EmployeeDBOperations.GetSalary("Akshay");
+            double expected = 25000;
             // Assert
             Assert.AreEqual(expected, actual);
         }
